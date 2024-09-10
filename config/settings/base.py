cd17d25e6a33b94d3fd10e939e806c842c1b88d7
cswitzer <chasehillig@yahoo.com>
2024-09-10 08:10:18 -0500

    feat: setup api, postgres, and mailpit containers

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from os import path
+from os import path, getenv
 
 from dotenv import load_dotenv
 
@@ -88,8 +88,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("POSTGRES_DB"),
+        "USER": getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "HOST": getenv("POSTGRES_HOST"),
+        "PORT": getenv("POSTGRES_PORT"),
     }
 }
 
