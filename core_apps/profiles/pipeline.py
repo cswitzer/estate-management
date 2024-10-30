3fc76fd081a94973f8ad51d8d450903e80fac7e0
cswitzer <chasehillig@yahoo.com>
2024-10-29 20:21:24 -0500

    add profile serializers and views

---
+++ core_apps/profiles/pipeline.py
@@ -0,0 +1,14 @@
+import cloudinary.uploader
+
+from core_apps.profiles.models import Profile
+
+
+# Referenced in SOCIAL_AUTH_PIPELINE. Sets the user's avatar to the one from their Google account.
+def save_profile(backend, user, response, *args, **kwargs):
+    if backend.name == "google-oauth2":
+        avatar_url = response.get("picture", None)
+        if avatar_url:
+            upload_result = cloudinary.uploader.upload(avatar_url)
+            profile, created = Profile.objects.get_or_create(user=user)
+            profile.avatar = upload_result["public_id"]
+            profile.save()
