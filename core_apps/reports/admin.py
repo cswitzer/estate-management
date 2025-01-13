05349f2d0db0514fb6af6f67e6b0d3fa38ad6d0d
cswitzer <chasehillig@yahoo.com>
2025-01-13 14:52:03 -0600

    add posts, upvote, downvote functionality

--- core_apps/reports/admin.py
+++ core_apps/reports/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from core_apps.reports.models import Report
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ["id", "reported_user", "reported_by", "created_at"]
+    search_fields = ["reported_user__username", "reported_by__username"]
+    list_filter = ["created_at"]
+    date_hierarchy = "created_at"
+    readonly_fields = ["id", "created_at"]
+    ordering = ["-created_at"]
