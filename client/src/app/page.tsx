eef40c6b76de00bdeb877b189f810aedd04dce62
cswitzer <chasehillig@yahoo.com>
2025-01-15 14:01:53 -0600

    configure prettier and eslint

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
 	return (
 		<div>
-			<h1 className='text-6xl'>Home Page</h1>
+			<h1 className="text-6xl font-robotoSlab">Home Page</h1>
+			<p className="text-3xl font-openSans">
+				There was this one man that was too greedy!
+			</p>
+			<p className="text-xs font-openSans bg-blue-50">Hello World!</p>
 		</div>
 	);
 }
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -14,8 +14,10 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
-			borderRadius: {},
-			colors: {},
+			fontFamily: {
+				openSans: ["var(--font-openSans)"],
+				robotoSlab: ["var(--font-robotoSlab)"],
+			},
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
