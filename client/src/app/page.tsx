e1fbedee95e508959b1f90bd93b4babcaa85c614
cswitzer <chasehillig@yahoo.com>
2025-01-20 14:06:07 -0600

    add register and landing pages

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,45 @@
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import buildings from "public/assets/images/buildings.webp";
+
+export const metadata: Metadata = {
+	title: "Home | Chase Apartments",
+	description: "Chas Apartments Home Page. Create your account to get started.",
+};
+
 export default function Home() {
 	return (
-		<div>
-			<h1 className="text-6xl font-robotoSlab">Home Page</h1>
-			<p className="text-3xl font-openSans">
-				There was this one man that was too greedy!
-			</p>
-			<p className="text-xs font-openSans bg-blue-50">Hello World!</p>
+		<div className="relative h-screen">
+			<div className="absolute inset-0 z-0">
+				<Image
+					src={buildings}
+					alt="Apartments"
+					fill
+					style={{ objectFit: "cover", objectPosition: "center" }}
+					priority
+				/>
+			</div>
+			<main className="flex-center relative z-10 h-full  bg-black/50">
+				<div className="text-center">
+					<h1 className="font-robotoSlab mb-4 text-4xl font-semibold text-cyan-400 antialiased sm:text-6xl md:text-8xl">
+						Welcome to Chase Apartments
+					</h1>
+					<p className="my-8 text-2xl text-teal-300 sm:text-4xl">
+						Are you a tenant? Or an existing tenant?
+					</p>
+					{/* We do not want to prefetch for our landing page */}
+					<Link href="/register" prefetch={false}>
+						<button className="bg-asparagus rounded-3xl px-4 py-2 text-lg font-semibold text-white hover:bg-lime-700 sm:px-6 sm:text-2xl">
+							<span className="inline-flex items-center">
+								Create Your Account
+								<ArrowRightIcon className="ml-2 size-6" />
+							</span>
+						</button>
+					</Link>
+				</div>
+			</main>
 		</div>
 	);
 }
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,18 +1,53 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
 	darkMode: ["class"],
 	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
 	],
+	prefix: "",
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			colors: {
+				googleBlue: "#4285F4",
+				asparagus: "#60992D",
+				deepBlueGrey: "#263238",
+				gray: "rgba(125,144,201,0.34)",
+				lightGray: "#7B8EC8",
+				electricIndigo: "#6610F2",
+				richBlack: "#0D1317",
+				veryBlack: "#000000",
+				babyPowder: "#FBFEF9",
+				pumpkin: "#F17105",
+				platinum: "#DDE1E4",
+				lightGrey: "#EEEEEE",
+				pear: "#C2E812",
+				eerieBlack: "#171D1C",
+				ballonWhite: "#dfe6f6",
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
 			},
 			fontFamily: {
 				openSans: ["var(--font-openSans)"],
@@ -21,5 +56,6 @@ const config: Config = {
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
-};
+} satisfies Config;
+
 export default config;
